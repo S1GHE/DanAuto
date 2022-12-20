f5367c9e5bb69f5e5aa5a93791e9258dd76094a7
s1ghe <golikoefim65@gmail.com>
2022-12-20 20:25:23 +0300

    edd

--- danAuto/danAuto/urls.py
+++ danAuto/danAuto/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 
 from django.urls import include
-from main_page.views import *
 
 urlpatterns = [
     path('', include('main_page.urls')),
--- danAuto/main_page/static/main_page/JS/app.js
+++ danAuto/main_page/static/main_page/JS/app.js
@@ -1,1 +1,8 @@
-window.document.write('<p>', 'JS подключен', '</p>')
+
+// function FunOnloud(){
+//     if(window.innerWidth <= 1000){
+//          let a = document.getElementsByClassName('sec_img col-10 col-sm-8 col-lg-6');
+//          a[0].style.display = 'none';
+//     }
+// }
+// window.onload = FunOnloud();
